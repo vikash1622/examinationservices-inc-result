02593a180f4d1254ba045de369b355a7db4eaa16
vikashyadav2004 <vikas@devwings.com>
2026-07-18 10:17:41 +0530

    code

--- src/componentnts/CandidateInfo.jsx
+++ src/componentnts/CandidateInfo.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import CanditateIMage from "../assets/candidate.jpg"
+import CanditateIMage from "../assets/candidate.png"
 import Qrcode from "../assets/qr-code.png"
 
 const CandidateInfo = () => {
